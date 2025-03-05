273463bcb1eabb4e193db17f202ffa2fae5689bc
gabriewl <gabriel.ferreiraf7@outlook.com>
2025-03-04 22:55:21 -0300

    adicionando ao github

---
+++ Módulo 1 (JavaScript Básico)/Aula 1 - #A006/oqVaiVerEmJS.js
@@ -0,0 +1,11 @@
+console.log("João Gabriel"); //Strings = Texto
+console.log('Gabriel'); 
+console.log(`'Ferreira' "Leite"`);
+
+console.log(17, 17.50, "Gabriewl"); //Number = Número
+
+function sla() {
+    let variavel = 12;
+    variavel2 = variavel + 2;
+}
+console.log(typeof sla);
