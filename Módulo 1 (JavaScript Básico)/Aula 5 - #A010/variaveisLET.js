6da8e2eed0a62d9aacfd8d715564e3d9fc796c7b
gabriewl <gabriel.ferreiraf7@outlook.com>
2025-03-07 22:32:52 -0300

    add aula 5

---
+++ Módulo 1 (JavaScript Básico)/Aula 5 - #A010/variaveisLET.js
@@ -0,0 +1,23 @@
+let nome = 'Gabriel'; //String
+let nome2 = 'Robson'; //String
+
+console.log(nome, 'nasceu em 1984.');
+console.log('Em 2000', nome2, 'conheceu Maria');
+console.log(nome, 'casou-se com Maria em 2012');
+console.log('Maria teve 1 filho com', nome, 'em 2015');
+console.log('O filho de', nome2, 'se chama Eduardo');
+console.log(nome, nome2);
+
+/*
+A palavra "João" é muito repetida nesses textos, eu queria de alguma maneira extrair esse nome e colocar na memória e sempre que eu precisar do nome eu vou referenciar este nome na memória. Então eu coloco uma variável com o valor de 'Gabriel' e 'Robson' e ao invés de colocar João eu coloco o nome da variável.
+*/
+
+/* 
+
+1) não podemos começar o nome de uma variável com número 
+2) variáveis precisam de nomes significativos para ter um entendimento melhor no código
+3) não podemos criar uma variável com palavras reservadas (if, else, console etc...)
+4) as variáveis não podem conter espaços ou traços (nome cliente X - nomeCliente V)
+5) Case-sensitive (Letra maiúscula importa no nome da variável. nomeCliente não é a mesma coisa que nomecliente)
+
+*/
