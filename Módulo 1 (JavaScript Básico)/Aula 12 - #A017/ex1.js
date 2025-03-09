1cb6a7b718a84d1f7cc0d7f55d0b665b379c3e8c
gabriewl <gabriel.ferreiraf7@outlook.com>
2025-03-09 20:14:04 -0300

    add aula 12

---
+++ Módulo 1 (JavaScript Básico)/Aula 12 - #A017/ex1.js
@@ -0,0 +1,20 @@
+/*
+let num1 = prompt('Digite um número');
+let num2 = prompt('Digite outro número');
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+alert(`O resultado da soma dos números é: ${num1 + num2}`); */
+
+// -------------------------------------------------------------------------------------------------------------
+
+let num1 = prompt('Digite um número');
+let num2 = prompt('Digite outro número');
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+const resultado = num1 + num2;
+
+alert(`A soma dos números é: ${resultado}`);
